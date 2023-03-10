dbbfffe0af0ebee50507da2cee0c8c310e75ce72
heidirogers <heidirogers17@gmail.com>
2023-03-10 12:21:38 -0600

    added test code for lab 7

---
+++ lab-07-GitTest.R
@@ -0,0 +1,7 @@
+# hello
+
+usethis::use_git_config(user.name = "heidirogers", user.email = "heidirogers17@gmail.com")
+usethis::create_github_token()
+gitcreds::gitcreds_set("https://github.com/heidirogers/stat-345")
+
+# this is test code
